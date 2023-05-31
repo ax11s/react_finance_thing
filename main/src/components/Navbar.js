fa5ca121dd3e568e8e954bdd91bd9e3c41fa9418
ax11 <125132362+ax11s@users.noreply.github.com>
2023-05-31 19:47:14 +0200

    gf

---
+++ main/src/components/Navbar.js
@@ -0,0 +1,9 @@
+export default function Navbar() {
+    return <nav class="w-full h-8 top-0">
+        <a href="/" >Fintool</a>
+
+        <a href="/debt" class="">Debt Calculator</a>
+        <a href="/investing" class="ml-2">Investing</a>
+
+    </nav>
+}
