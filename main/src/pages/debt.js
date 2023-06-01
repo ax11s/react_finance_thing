6f22738bac9adf4b0ca04bed4632e27ff3098b00
ax11 <125132362+ax11s@users.noreply.github.com>
2023-06-01 11:17:29 +0200

    Update debt.js

--- main/src/pages/debt.js
+++ main/src/pages/debt.js
@@ -1,5 +1,42 @@
+import React, { useState } from 'react';
+
 export default function Debt () {
+    const [input1, setInput1] = useState('');
+    const [input2, setInput2] = useState('');
+    const [input3, setInput3] = useState('');
+    const [sum, setSum] = useState(0);
+  
+    const handleSubmit = (e) => {
+      e.preventDefault();
+      const num1 = parseInt(input1);
+      const num2 = parseInt(input2);
+      const num3 = parseInt(input3);
+      const total = num1 + num2 + num3;
+      setSum(total);
+    };
     return (
-        <h1>debt page</h1>
+        <div class="w-full">
+
+            <div className="container mt-16 mx-auto p-4">
+      <h1 className="text-2xl font-bold mb-4">Debt Calculator</h1>
+      <form onSubmit={handleSubmit} className="mb-4">
+        <div className="flex items-center mb-2">
+          <label htmlFor="input1" className="mr-2">Amount</label>
+          <input type="number" id="input1" value={input1} onChange={e => setInput1(e.target.value)} className="border p-2" required />
+        </div>
+        <div className="flex items-center mb-2">
+          <label htmlFor="input2" className="mr-2">Period (in Years)</label>
+          <input type="number" id="input2" value={input2} onChange={e => setInput2(e.target.value)} className="border p-2" required />
+        </div>
+        <div className="flex items-center mb-2">
+          <label htmlFor="input3" className="mr-2">Inflation (in %)</label>
+          <input type="number" id="input3" value={input3} onChange={e => setInput3(e.target.value)} className="border p-2" required />
+        </div>
+        <button type="submit" className="bg-orange-500 text-white px-4 py-2 rounded">Submit</button>
+      </form>
+      {sum !== 0 && <p className="text-lg font-semibold">Sum: {sum}</p>}
+      {sum !== 0 && <p className="text-lg font-semibold">Sum: {sum}</p>}
+    </div>
+        </div>
     );
 }
